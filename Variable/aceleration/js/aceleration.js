a24f5b7ce6fd81ad98e82a4258564da10f6f635f
Esteban_Ome <juanome43@gmail.com>
2025-03-19 13:44:35 -0500

    JS_basics

---
+++ Variable/aceleration/js/aceleration.js
@@ -0,0 +1,13 @@
+/*
+	name exercise: aceleration
+	description: aceleration of a car
+	autor: Esteban Ome
+	date: march 15th 2025
+*/
+let aceleration;
+let force=200;
+let mass=40;
+
+aceleration =force/mass
+
+console.log("The aceleration is "+aceleration);

d2c38324e4e552cf7401f431fee030ac191b5884
Esteban1807 <juanome43@gmail.com>
2025-03-20 07:49:29 -0500

    planets.js

--- cycle_while/planets/js/planets.js
+++ cycle_while/planets/js/planets.js
@@ -11,30 +11,28 @@ let planet;
 let weight = 0;
 let totalWeight = 0;
 let counter = -1;
+bulk = parseFloat(prompt("Write the bulk (negative to exit): "));
 
 while (bulk >= 0) {
     counter++;
-    bulk = parseFloat(prompt("Write the bulk (negative to exit): "));
-   
-    if (bulk < 0) {
-        break;
-    }
 
     planet = prompt("Select a planet (1 for Earth, 2 for Mars, 3 for Jupiter): ");
     if (planet == 1) {
         weight = bulk * 9.8;
+	console.log("Weight in Earth: " + weight);    
     } else if (planet == 2) {
         weight = bulk * 3.71;
+	console.log("Weight in Mars: " + weight);    
     } else if (planet == 3) {
         weight = bulk * 24.79;
+	console.log("Weight in Jupiter: " + weight);    
     } else {
         console.log("Invalid number");
         continue;
     }
 
     totalWeight += weight;
-
-    console.log("Weight: " + weight);
+    bulk = parseFloat(prompt("Write the bulk (negative to exit): "));
 }
 
 console.log("The total weight: " + totalWeight);
